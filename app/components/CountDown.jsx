55adae65a5101b08ef39b810f6ff9b0d71207d7f
Marin Rusi <m_rusi@yahoo.com>
2017-11-05 11:50:41 -0500

    clock styles

--- app/components/CountDown.jsx
+++ app/components/CountDown.jsx
@@ -1,10 +1,11 @@
 var React = require('react');
+var Clock = require('Clock');
 
 var CountDown =  (props) => {
     return (
         <div>
         <h1 className="text-center page-title">CountDown</h1>
-        
+            <Clock totalSeconds = {129} />
         </div>
     )
 };
