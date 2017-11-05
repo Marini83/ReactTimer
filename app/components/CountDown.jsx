b9047e9878c6d51bacf547d8007a084c9c763e1a
Marin Rusi <m_rusi@yahoo.com>
2017-11-04 20:35:52 -0400

    timer & countdown2

---
+++ app/components/CountDown.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+
+var CountDown =  (props) => {
+    return (
+        <div>
+        <h1 className="text-center page-title">CountDown</h1>
+        
+        </div>
+    )
+};
+
+module.exports = CountDown;
