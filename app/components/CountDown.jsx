daefc687f67c89ff3a694a700e00e900603637ab
Marin Rusi <m_rusi@yahoo.com>
2017-11-05 22:58:23 -0500

    changes to countdown form

--- app/components/CountDown.jsx
+++ app/components/CountDown.jsx
@@ -1,13 +1,45 @@
 var React = require('react');
 var Clock = require('Clock');
+var CountdownForm = require('CountdownForm');
 
-var CountDown =  (props) => {
+var Countdown = React.createClass({
+    getInitialState: function() {
+        return {count: 0,
+        countdownStatus: 'stopped'
+        };
+    },
+    startTimer: function() {
+        this.timer = setInterval(() => {
+            var newCount = this.state.count - 1;
+            this.setState ({
+                count: newCount >= 0 ? newCount : 0
+            });
+        }, 1000);
+    },
+    componentDidUpdate: function (prevProps, prevState) {
+        if (this.state.countdownStatus !== prevState.countdownStatus){
+            switch(this.state.countdownStatus){
+                case 'started':
+                this.startTimer();
+                break; 
+            }
+        } 
+    },
+    handleSetCountdown: function (seconds){
+        this.setState({
+            count: seconds,
+            countdownStatus: 'started'
+        });
+    },
+    render: function() {
+        var {count} = this.state;
     return (
         <div>
-        <h1 className="text-center page-title">CountDown</h1>
-            <Clock totalSeconds = {129} />
+            <Clock totalSeconds = {count} />
+            <CountdownForm onSetCountdown={this.handleSetCountdown}/>
         </div>
-    )
-};
+    );
+    }
+});
 
-module.exports = CountDown;
+module.exports = Countdown;
--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,8 @@ module.exports = {
 			Nav: 'app/components/Nav.jsx',
 			Timer: 'app/components/Timer.jsx',
 			CountDown: 'app/components/CountDown.jsx',
-			Clock: 'app/components/Clock.jsx',			
+			Clock: 'app/components/Clock.jsx',	
+			CountdownForm: 'app/components/CountdownForm.jsx',		
 			applicationStyles: 'app/styles/app.scss'
 		},
 		extensions: ['','.js','.jsx']
